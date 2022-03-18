aed5d5f0677bc3c01ca6b00ca7a9c454b5f0b10e
kauzki-77 <k.sora.7861@gmail.com>
2022-03-19 00:58:17 +0900

    [add]コンストラクト引数練習

--- cap8/Main.java
+++ cap8/Main.java
@@ -3,8 +3,8 @@ package cap8;
 public class Main {
   public static void main(String[] args) {
     // 勇者を生成
-    Hero h1 = new Hero(); // Heroクラスからインスタンスを生成し、変数h1に入れる
-    Hero h2 = new Hero();
+    Hero h1 = new Hero("ミナト"); // Heroクラスからインスタンスを生成し、変数h1に入れる
+    Hero h2 = new Hero("アサカ");
 
     // お化けキノコを生成
     Matango m1 = new Matango();
@@ -14,13 +14,19 @@ public class Main {
     Sword s = new Sword();
     s.name = "炎の剣";
     s.damage = 10;
+
+    Sword s2 = new Sword();
+    s2.name = "氷の剣";
+    s2.damage = 12;
+
     // フィールドに初期値をセット
-    h1.name = "ミナト";
+    // h1.name = "ミナト";
     h1.hp = 100;
     h1.sword = s;
 
-    h2.name = "アサカ";
+    // h2.name = "アサカ";
     h2.hp = 100;
+    h2.sword = s2;
 
     Wizard w = new Wizard();
     w.name = "スガワラ";
@@ -39,7 +45,11 @@ public class Main {
     m1.run();
     m2.run();
     h1.run();
-    System.out.println("勇者" + h1.name + "を生み出しました！");
+    System.out.println("");
+    System.out.println("勇者" + h2.name + "を生み出しました！");
+    System.out.println("現在の武器は" + h2.sword.name + "です");
+    System.out.println(h2.name + "のHPは、" + h2.hp + "です");
+
     // 勇者のメソッドを呼び出してゆく
     h1.sit(5); // 5秒座れ
     h1.slip(); // 転べ
@@ -50,6 +60,7 @@ public class Main {
     w.heal(h1); // ミナトのHPを回復させる (HP: 100 → 110)
     w.heal(h2); // アサカのHPを回復させる (HP: 100 → 110)
     w.heal(h2); // アサカのHPを回復させる (HP: 110 → 120)
+    System.out.println(h2.name + "のHPは" + h2.hp + "です");
   }
 
 }
