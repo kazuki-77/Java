bfb8a7dfac50d5bd1d46ec9d741e12bddc767020
kauzki-77 <k.sora.7861@gmail.com>
2022-03-19 17:35:30 +0900

    [add]親インスタンスへのアクセス

--- cap10/SuperHero.java
+++ cap10/SuperHero.java
@@ -5,6 +5,13 @@ package cap10;
 // Heroと同じメンバの定義は省略する(違いだけを記述する)」という意味となる。
 public class SuperHero extends Hero{ // 基本的にはHeroと同じ宣言
   boolean flying; // 追加したflying
+  public void attack(Matango m) {
+    // superは今より一つ内側のインスタンス部分を表す予約語
+    super.attack(m); // 親インスタンス部分の attack()を呼び出し
+    if(this.flying) {
+      super.attack(m); // 親インスタンス部分の attack()を呼び出し
+    }
+  }
   public void fly() { // 追加したfly()
     this.flying = true;
     System.out.println("飛び上がった");
@@ -14,3 +21,9 @@ public class SuperHero extends Hero{ // 基本的にはHeroと同じ宣言
     System.out.println("着地した");
   }
 }
+//  親インスタンス部分のフィールドを利用する
+//  super.フィールド名
+//  親インスタンス部分のメソッドを呼び出す
+//  super.メソッド名(引数)
+//  superを付けないとthis.attack()などと同じ意味になってしまうため、
+//  必ず一つ内側のインスタンスを呼び出すときはsuperをつける
